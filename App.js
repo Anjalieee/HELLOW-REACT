7ac3a099e74fe4e9f055eac7a9a068e0b5d17869
Anjalieee <anjalisawant737@gmail.com>
2026-05-28 17:15:17 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
-const parent =React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child"}, [
-        React.createElement("h1", {}, "I am an h1 tag"),
-        React.createElement("h2", {}, "I am an h2 tag")]
-    ),
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {createRoot} from "react-dom/client";
 
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "I am an h1 tag"),
-        React.createElement("h2", {}, "I am an h2 tag"),
-        React.createElement("h6", {}, "I am an h6 tag")]
-    )
+const parent = React.createElement(
+    "div",
+    { id: "parent", key: "parent" },
+    [
+        React.createElement(
+            "div",
+            { id: "child", key: "child-1" },
+            [
+                React.createElement("h1", { key: "h1-1" }, "I am an h1 tag"),
+                React.createElement("h2", { key: "h2-1" }, "I am an h2 tag")
+            ]
+        ),
 
-]);
+        React.createElement(
+            "div",
+            { id: "child2", key: "child-2" },
+            [
+                React.createElement("h1", { key: "h1-2" }, "I am an h1 tag"),
+                React.createElement("h2", { key: "h2-2" }, "I am an h2 tag"),
+                React.createElement("h6", { key: "h6-1" }, "I am an h6 tag")
+            ]
+        )
+    ]
+);
 
 console.log(parent);
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 Hello-React by Anjali Sawant.
+
+#Parcel
+Dev-build
+local server
+HMR- Hot module Replacement
+File watching algo- written in c++
+CAching- faster builds
+minification
+bundling
+image optimisation
+compressing
+consistent hashing
+code splitting
+diagnostics and good error handling
