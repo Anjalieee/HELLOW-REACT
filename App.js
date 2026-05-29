1c72a737cbcb58c48fe7b79437dd9bc990cef0ba
Anjalieee <anjalisawant737@gmail.com>
2026-05-29 15:59:59 +0530

    Episode 4 updated

--- App.js
+++ App.js
@@ -1,32 +1,73 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {createRoot} from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent", key: "parent" },
-    [
-        React.createElement(
-            "div",
-            { id: "child", key: "child-1" },
-            [
-                React.createElement("h1", { key: "h1-1" }, "I am an h1 tag"),
-                React.createElement("h2", { key: "h2-1" }, "I am an h2 tag")
-            ]
-        ),
 
-        React.createElement(
-            "div",
-            { id: "child2", key: "child-2" },
-            [
-                React.createElement("h1", { key: "h1-2" }, "I am an h1 tag"),
-                React.createElement("h2", { key: "h2-2" }, "I am an h2 tag"),
-                React.createElement("h6", { key: "h6-1" }, "I am an h6 tag")
-            ]
-        )
-    ]
-);
+const Header = () =>{
+    return (
+        <div className="header">
+            <nav className="Navbar">
+                <div className="logo-container">
+                    <img className="logo" src="https://cdn.dribbble.com/userupload/24473389/file/original-ecc8c127362397f9d83ebfaebccdbbc7.png?resize=400x0"></img>
+                </div> 
+                <div className="nav-items">
+                    <ul>
+                        <li>Dining Out</li>
+                        <li>Delivery</li>
+                        <li>Nightlife</li>
+                        <li>Cart</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
 
-console.log(parent);
-const root = createRoot(document.getElementById("root"));
-root.render(parent);
+const stylecard ={
+    backgroundColor : "#f0f0f0"
+}
+
+const RestaurantCard = (props) => {
+    const {resData} = props;
+
+    const {image, name, cuisine, rating, cft, locality} = resData?.info;
+
+    return (
+        <div className="res-card" style={stylecard}>
+            <img
+            className="res-logo"
+            alt="res-logo" src={image.url}></img>
+            <h3>{name}</h3>
+            <h4>{cuisine.map((c) => c.name).slice(0, 4).join(", ")} | {rating.aggregate_rating} stars</h4>
+            <h4>{cft.text}</h4>
+            <h5>{locality.name}</h5>
+        </div>
+    )
+}
+/*
+const resObj = []*/
+
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {resObj.map((restaurant) =>(<RestaurantCard key={restaurant.info.resId} resData={restaurant}/>))}
+            </div>
+
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+        <Header/>
+        <Body/>
+        </div>
+
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
